b5c0fbb2e6c9184770cdeb32167ca3eccc449520
learGitHub <imintseng6@gmail.com>
2019-06-23 00:49:11 +0000

    02_44.Sending email Mailing production productionrb modification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  ActionMailer::Base.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { host: 'nomster02-imin-tseng.herokuapp.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
